ce7e8511fdbecc093a5e024ce3205aad8026c2ff
ondrej <ondrej@147.213.205.185>
2018-12-03 13:59:36 +0100

    Url validation

---
+++ paz1cProjekt/src/main/java/util/Utils.java
@@ -0,0 +1,32 @@
+package util;
+
+import java.net.URL;
+
+public class Utils {
+
+	/* Returns true if url is valid */
+	public static boolean isValidURL(String url) {
+		/* Try creating a valid URL */
+		try {
+			new URL(url).toURI();
+			return true;
+		}
+
+		// If there was an Exception
+		// while creating URL object
+		catch (Exception e) {
+			return false;
+		}
+	}
+
+	public static String parseUrl2UrlMapBaseFormat(String url) {
+		String[] tokens = url.split("/");
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < tokens.length - 3; i++) {
+			sb.append(tokens[i]);
+			sb.append("/");
+		}
+
+		return sb.toString();
+	}
+}
---
+++ paz1cProjekt/src/test/java/util/UtilsTest.java
@@ -0,0 +1,14 @@
+package util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class UtilsTest {
+	
+	@Test
+	void TestparseUrl2UrlMapBaseFormat(){
+		String urlString = "https://static.mapy.hiking.sk/topo/13/4530/2826.png";
+		assertEquals("https://static.mapy.hiking.sk/topo/", Utils.parseUrl2UrlMapBaseFormat(urlString));
+	}
+}
